9202731bf6381dafd449f36030d9d0fc97f710fe
Ratimon <ratixoxo@gmail.com>
2023-09-24 19:58:38 +0700

    Feat & Test (./): Adding add()

--- python_ecdsa/elliptic_curve.py
+++ python_ecdsa/elliptic_curve.py
@@ -1,4 +1,4 @@
-# from py_ecc.bn128 import G1, multiply, add, eq
+from py_ecc.bn128 import G1, multiply, add, eq
 
 # This represents an elliptic curve of the form
 #  y^2 = x^3 + ax + b mod p
@@ -11,3 +11,42 @@ class EllipticCurve():
     def is_on_curve(self, point):
         x, y = point
         return (y**2 % self.p) == (x**3 + self.a * x + self.b) % self.p
+
+
+# m = (3 * x1**2 + self.a) * pow(2 * y1, -1, self.p)
+
+
+    def add(self, point1, point2):
+        if point1 is None:
+            return point2
+        if point2 is None:
+            return point1
+
+        x1, y1 = point1
+        x2, y2 = point2
+
+
+
+        # vertical
+        if x1 == x2 and y1 + y2 == 0 :
+            return None  # Identity element
+        
+        if point1 == point2:
+            m = (3 * x1**2 + self.a) * pow(2 * y1, -1, self.p)
+        else:
+            if x1 == x2:
+                return None   # Identity element
+            m = (y2 - y1) * pow(x2 - x1, -1, self.p)
+
+        x3 = (m**2 - x1 - x2) % self.p
+        y3 = (m * (x1 - x3) - y1) % self.p
+
+        return (x3, y3)
+    
+
+    
+    def double(self, point):
+        return multiply(point, 2)
+
+    def scalar_mul(self, point, scalar):
+        return multiply(point, scalar)
--- tests/test_elliptic_curve.py
+++ tests/test_elliptic_curve.py
@@ -1,10 +1,73 @@
 from python_ecdsa.elliptic_curve import EllipticCurve
+from py_ecc.bn128 import  multiply, add, eq
 
+def test_is_on_curve():
+    ec = EllipticCurve(2, 2, 17)
+    assert ec.is_on_curve((5, 1))
+    assert not ec.is_on_curve((5, 3))  # A point not on the curve
 
-def test_version():
+from py_ecc.bn128 import eq
 
+def test_add():
     # y^2 = x^3 + 2x + 2 mod 17
-    ec = EllipticCurve(2,2,17)
+    ec = EllipticCurve(2, 2, 17)
 
-    assert ec.is_on_curve( (5,1) )
+    # (6,3) + (5,1) = (10,6)
+    point1 = (6, 3)
+    point2 = (5, 1)
+    point3 = (10, 6)
+    result = ec.add(point1, point2)
+    assert eq(result, point3)
+
+    # (5,1) + (5,1) = (6,3)
+    point1 = (5, 1)
+    point2 = (5, 1)
+    point3 = (6, 3)
+    result = ec.add(point1, point2)
+    assert eq(result, point3)
+
+    # (10, 6) + (5, 1) = (3,1)
+    point1 = (10, 6)
+    point2 = (5, 1)
+    point3 = (3, 1)
+    result = ec.add(point1, point2)
+    assert eq(result, point3)
+
+    # (16, 13) + (5, 1) = (0, 6)
+    point1 = (16, 13)
+    point2 = (5, 1)
+    point3 = (0, 6)
+    result = ec.add(point1, point2)
+    assert eq(result, point3)
+
+    # (6,3) + I = (6,3)
+    point1 = (6, 3)
+    result = ec.add(point1, None)
+    assert eq(result, point1)
+
+    # I + I = 2 * I = I
+    result = ec.add(None, None)
+    assert result is None
+
+    # (5,16) + (5,1) = I
+    point1 = (5, 16)
+    point2 = (5, 1)
+    result = ec.add(point1, point2)
+    assert result is None
+
     
+
+# def test_double():
+#     ec = EllipticCurve(2, 2, 17)
+#     point = (5, 1)
+#     result = ec.double(point)
+#     expected_result = multiply(point, 2)
+#     assert eq(result, expected_result)
+
+# def test_scalar_mul():
+#     ec = EllipticCurve(2, 2, 17)
+#     point = (5, 1)
+#     scalar = 3
+#     result = ec.scalar_mul(point, scalar)
+#     expected_result = multiply(point, scalar)
+#     assert eq(result, expected_result)
