d7b20a52a643efaf0465c13c9cb58623676b93de
Ratimon <ratixoxo@gmail.com>
2023-09-27 07:56:56 +0700

    Test (./tests): Adding case for secp256

--- tests/test_ecdsa.py
+++ tests/test_ecdsa.py
@@ -17,26 +17,18 @@ def test_sign_verify():
     # Generate a random private key
     # priv_key = ecdsa.generate_priv_key()
    
-
     # Generate a public key
-    public_key = ecdsa.generate_pub_key(priv_key)
-    # priv_key, public_key =ecdsa.generate_key_pair()
+    pub_key = ecdsa.generate_pub_key(priv_key)
+    # priv_key, pub_key =ecdsa.generate_key_pair()
 
     k_random = int(18) 
     # Generate a random value for k
     # k_random = random.randint(1, curve_order - 1)
 
-    # Generate a message
     message = "Bob -> 1 BTC -> Alice"
-
-    # Compute the hash of the message
     hash_value = ecdsa.generate_hash_less_than(message, ecdsa.curve_order)
-
-    # Sign the message
     signature = ecdsa.sign(hash_value, priv_key, k_random)
-
-    # Verify the signature
-    is_valid = ecdsa.verify(hash_value, public_key, signature)
+    is_valid = ecdsa.verify(hash_value, pub_key, signature)
 
     assert is_valid, "Verification should success"
 
@@ -65,3 +57,31 @@ def test_revert_when_tempered_msg_sign_verify():
     verify_result = ecdsa.verify(hash_value2, pub_key, signature)
 
     assert not verify_result, "Verification should fail when the message is tempered"
+
+def test_secp256_sign_verify():
+    
+    p_hex = "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F"
+    curve_order_hex = "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141"
+
+    gx_hex = "79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798"
+    gy_hex = "483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8"
+
+    a = 0
+    b = 7
+    p = int(p_hex, 16)
+    curve_order = int(curve_order_hex, 16)
+    gen = (int(gx_hex, 16), int(gy_hex, 16))
+
+    ecdsa = ECDSA(a, b, p, gen, curve_order)
+
+    priv_key = int(8)
+    pub_key = ecdsa.generate_pub_key(priv_key)
+    k_random_hex = "19BE666EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B15E81798"
+    k_random = int(k_random_hex, 16)
+
+    message = "Bob -> 1 BTC -> Alice"
+    hash_value = ecdsa.generate_hash_less_than(message, ecdsa.curve_order)
+    signature = ecdsa.sign(hash_value, priv_key, k_random)
+    is_valid = ecdsa.verify(hash_value, pub_key, signature)
+
+    assert is_valid, "Verification should success"
